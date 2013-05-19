3d9746665ff27da9ad66e64503865b616c52e784
Simon van Dyk <simon.vandyk@gmail.com>
2013-05-19 20:13:40 +0200

    Init nbproject files.

---
+++ GPFinance/src/gpfinance/GPFinance.java
@@ -0,0 +1,12 @@
+
+package gpfinance;
+
+/**
+ * @date   2013-06-01
+ * @author Simon van Dyk, Stuart Reid
+ */
+public class GPFinance {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ GPFinance/src/gpfinance/Simulator.java
@@ -0,0 +1,17 @@
+
+package gpfinance;
+
+/**
+ * @date   2013-06-01
+ * @author Simon van Dyk, Stuart Reid
+ */
+public class Simulator {
+    
+    public Simulator(){
+        
+    }
+    
+    public void run(){
+        
+    }
+}
