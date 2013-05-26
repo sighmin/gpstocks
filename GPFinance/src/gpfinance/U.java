4e38ef9a2cd13850285100bebc60adbff1f2b970
Stuart Reid <stuart@sinkwa.com>
2013-05-26 11:15:25 +0200

    Changed some stuff in GP Finance and added Fund enums

--- GPFinance/src/gpfinance/U.java
+++ GPFinance/src/gpfinance/U.java
@@ -5,10 +5,11 @@ import java.security.SecureRandom;
 import java.util.Random;
 
 /**
- * @date   2013-06-01
+ * @date 2013-06-01
  * @author Simon van Dyk, Stuart Reid
  */
 public class U {
+
     /**
      * Utility class configuration parameters.
      */
@@ -16,47 +17,81 @@ public class U {
     public static final double MIN = -100.0;
     public static final boolean debug = true;
     public static double MEAN = 0.0;
-    
+    public static Random randomGenerator = new Random();
+
     /**
-     * Set of methods to print the object passed.
-     *   p()  - prints object with no new line, if debugging
-     *   pl() - prints object with a new line, if debugging
-     *   m()  - prints the object regardless of debug mode
+     * Set of methods to print the object passed. p() - prints object with no
+     * new line, if debugging pl() - prints object with a new line, if debugging
+     * m() - prints the object regardless of debug mode
      */
-    public static void p(Object o){ if (debug) System.out.print(o); }
-    public static void pl(Object o){ if (debug) System.out.println(o); }
-    public static void pl(){ if (debug) System.out.println(); }
-    public static void m(Object o){ System.out.println(o); }
-    
+    public static void p(Object o) {
+        if (debug) {
+            System.out.print(o);
+        }
+    }
+
+    public static void pl(Object o) {
+        if (debug) {
+            System.out.println(o);
+        }
+    }
+
+    public static void pl() {
+        if (debug) {
+            System.out.println();
+        }
+    }
+
+    public static void m(Object o) {
+        System.out.println(o);
+    }
+
     /**
-     * Set of methods to help with random generation of data types.
-     *   chance()    - 50/50 chance to return true/false
-     *   randomVal() - return random double in range [MIN, MAX)
+     * Set of methods to help with random generation of data types. chance() -
+     * 50/50 chance to return true/false randomVal() - return random double in
+     * range [MIN, MAX)
      */
-    public static boolean chance(){ return Math.random() < 0.5; }
-    public static double r() { return Math.random(); }
-    public static int rint(int size) { return new Random().nextInt(size); }
-    public static double randomVal() { return (MIN + (Math.random() * ((MAX - MIN) + 1.0)) * 0.85); }
-    public static int randomTreeIndex(int size) { return (new Random()).nextInt(size-1) +1; }
-    
-    public static double getRandomGauss(double... DEVIATION){
-        if (DEVIATION.length == 0){ DEVIATION = new double[1]; DEVIATION[0] = 0.1; }
-        double q, u, v, x, y;
-        SecureRandom random = new SecureRandom();
-        do {
-            u = random.nextDouble(); v = random.nextDouble();
-            if (u <= 0.0 || v <= 0.0) {
-                u = 1.0;
-                v = 1.0;
-            }
-            v = 1.7156 * (v - 0.5);
-            x = u - 0.449871;
-            y = Math.abs(v) + 0.386595;
-            q = x * x + y * (0.19600 * y - 0.25472 * x);
-            if (q < 0.27597) { break; }
-        } while ((q > 0.27846) || (v * v > -4.0 * Math.log(u) * u * u));
+    public static boolean chance() {
+        return Math.random() < 0.5;
+    }
+
+    public static double r() {
+        return Math.random();
+    }
+
+    public static int rint(int size) {
+        return new Random().nextInt(size);
+    }
+
+    public static double randomVal() {
+        return (MIN + (Math.random() * ((MAX - MIN) + 1.0)) * 0.85);
+    }
+
+    public static int randomTreeIndex(int size) {
+        return (new Random()).nextInt(size - 1) + 1;
+    }
+
+    public static double getRandomGauss(double... DEVIATION) {
+        /*if (DEVIATION.length == 0){ DEVIATION = new double[1]; DEVIATION[0] = 1.0; }
+         double q, u, v, x, y;
+         SecureRandom random = new SecureRandom();
+         do {
+         u = random.nextDouble(); v = random.nextDouble();
+         if (u <= 0.0 || v <= 0.0) {
+         u = 1.0;
+         v = 1.0;
+         }
+         v = 1.7156 * (v - 0.5);
+         x = u - 0.449871;
+         y = Math.abs(v) + 0.386595;
+         q = x * x + y * (0.19600 * y - 0.25472 * x);
+         if (q < 0.27597) { break; }
+         } while ((q > 0.27846) || (v * v > -4.0 * Math.log(u) * u * u));
         
-        return (MEAN + DEVIATION[0] * v / u);
+         System.out.println(MEAN + DEVIATION[0] * v / u);
+         return (MEAN + DEVIATION[0] * v / u);*/
+
+        return (randomGenerator.nextGaussian() * 100);
     }
 
 }
